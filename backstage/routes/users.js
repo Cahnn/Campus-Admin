f4893a5fae3b9e7bc4beb7eef2d896acab00f016
aoteman-web <t1781843976@qq.com>
2020-12-16 23:15:30 +0800

    first commit

---
+++ backstage/routes/users.js
@@ -0,0 +1,18 @@
+const router = require('koa-router')()
+//
+const user = require('../db/models/user')
+router.prefix('/users')
+
+router.get('/add', function (ctx, next) {
+  ctx.body = 'this id a users/bar response'
+})
+
+router.get('/', function (ctx, next) {
+  ctx.body = 'this is a users response!'
+})
+
+router.get('/bar', function (ctx, next) {
+  ctx.body = 'this is a users/bar response'
+})
+
+module.exports = router
